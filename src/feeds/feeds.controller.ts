06f1a07cec471e9e7a675800435ac6f223c1874e
zuyo7852 <leejuyeon7852@gmail.com>
2025-08-20 13:18:38 +0900

    #4 feed API

--- src/feeds/feeds.controller.ts
+++ src/feeds/feeds.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards, Post, Body, Req, Delete, Param} from '@nestjs/common';
 import { FeedsService } from './feeds.service';
+import { JwtAuthGuard } from 'src/jwt-auth.guard';
+import { CreateFeedDto } from './dto/create-feed.dto';
 
+@UseGuards(JwtAuthGuard)
 @Controller('feeds')
 export class FeedsController {
     constructor(private readonly feedService:FeedsService) {}
 
     @Get()
     getFeeds(){
-        return this.feedService.getFeeds();
+        return this.feedService.getFeedWithUsers();
+    }
+
+    @Post()
+    createdFeed(@Body() feed:CreateFeedDto, @Req() request:Request & {user: {id:number}}, )
+    {
+        const userId = request.user.id;
+        return this.feedService.createdFeed({...feed}, userId);
+    }
+
+    @Delete(':id')
+    deleteFeed(@Param('id') id:number){
+        return this.feedService.deleteFeed(id);
     }
 }
--- src/feeds/feeds.module.ts
+++ src/feeds/feeds.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { FeedsController } from './feeds.controller';
 import { FeedsService } from './feeds.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { Feed } from './feed.entity';
+import { User } from 'src/users/user.entity';
+
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Feed, User]),
+    JwtModule.register({
+          secret: 'random-secret-key',
+          signOptions: {expiresIn: '600m'},
+    }),
+  ],
   controllers: [FeedsController],
   providers: [FeedsService]
 })
